e9957677940ec31376abf8925f6b46f1bebe25fd
kaelaclement <stephanie.kaela@gmail.com>
2019-01-18 17:43:19 -0800

    add bcrypt to Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+kaela = User.create(email: "kaela@howtoveg.com", password: "test")
+lorna = User.create(email: "lorna@howtoveg.com", password: "test")
+
+kaelas_recipe = Recipe.create(author: kaela, title: "shakshouka", ingredients: "shakshouka stuff", instructions: "make it")
+lornas_recipe = Recipe.create(author: lorna, title: "bagel", ingredients: "bagel, cream cheese", instructions: "toast the bagel maybe. put on the cream cheese")
+
+Review.create(user: kaela, recipe: lornas_recipe, liked?: false, comment: "did we need a recipe?")
+Review.create(user: lorna, recipe: kaelas_recipe, liked?: true, comment: "i love this stuff!")
