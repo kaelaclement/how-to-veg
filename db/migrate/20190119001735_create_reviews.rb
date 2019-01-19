f84231ab2c3bf5afc88f386f31ba7bba78e0b9d3
kaelaclement <stephanie.kaela@gmail.com>
2019-01-18 17:41:03 -0800

    create reviews table

---
+++ db/migrate/20190119001735_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :recipe_id
+      t.string :comment
+      t.boolean :liked?
+
+      t.timestamps
+    end
+  end
+end
