bc260046a90962e05ef1e0622035dd150e13df86
kaelaclement <stephanie.kaela@gmail.com>
2019-01-18 18:59:15 -0800

    add resource routes for users, recipes, and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
-  get '/signup', to: 'users#signup'
-  post '/signup', to: 'users#create'
+  resources :users do
+    resources :recipes
+    resources :reviews, only: [:index]
+  end
 end
