8f92814b5e3f5a231b0199150af36766c14a3597
kaelaclement <stephanie.kaela@gmail.com>
2019-01-21 15:38:59 -0800

    create login and logout actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      # or do I want a specific user homepage??
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Please check email and password and try again"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
 end
