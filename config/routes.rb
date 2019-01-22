184413d60ac36402336dd3f6643b1b184cf870fe
kaelaclement <stephanie.kaela@gmail.com>
2019-01-22 13:18:35 -0800

    create new review form on recipe show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+  post '/reviews', to: 'reviews#create'
 end
