552569b73b3edb86af086293a3f27d1d9d9dee25
kaelaclement <stephanie.kaela@gmail.com>
2019-01-21 19:04:35 -0800

    add name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_001735) do
+ActiveRecord::Schema.define(version: 2019_01_22_024909) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_01_19_001735) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
