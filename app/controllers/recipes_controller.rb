9dedb35fae42df2148c798b412bf939c2e28a200
kaelaclement <stephanie.kaela@gmail.com>
2019-01-21 20:46:08 -0800

    add index action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+  def index
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      @recipes = @user.authored_recipes      
+    else
+      @recipes = Recipe.all
+    end
+  end
 end
