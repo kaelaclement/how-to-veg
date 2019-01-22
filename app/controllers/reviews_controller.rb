da3e512bef91af96cb04307e7f6393bf791f258e
kaelaclement <stephanie.kaela@gmail.com>
2019-01-22 13:21:48 -0800

    add strong params for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,11 @@
 class ReviewsController < ApplicationController
   def create
+    
   end
+
+  private
+
+    def reivew_params
+      params.require(:review).permit(:like, :comment)
+    end
 end
