099c2e4077d66f4f5df0c3f8380d91f1a21e80e5
kaelaclement <stephanie.kaela@gmail.com>
2019-01-21 19:05:50 -0800

    add validation for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true
+  validates :name, presence: true
 end
