7ca10aa168a38c3df5115c7cc26ff7cc7ee4f562
kaelaclement <stephanie.kaela@gmail.com>
2019-01-27 20:12:29 -0800

    refactor recipe show page

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -6,4 +6,10 @@ module RecipesHelper
       "Browse all recipes here!"
     end
   end
+
+  def edit_if_author
+    if @recipe.author == current_user
+      link_to "Edit this recipe", edit_user_recipe_path(@recipe.author, @recipe)
+    end
+  end
 end
