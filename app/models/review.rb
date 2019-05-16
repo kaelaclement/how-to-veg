4a0c35a116c8eaa5d4ca4fee5979620b6756c62c
kaelaclement <stephanie.kaela@gmail.com>
2019-05-15 21:31:11 -0700

    Add jquery

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,6 +9,10 @@ class Review < ApplicationRecord
     group(:recipe_id).order(:like).sum(:like)
   end
 
+  # most_liked_hash.sort_by {|k, v| v}
+  # returns sorted array
+  # array to hash?
+
   def self.user_liked_reviews(user)
     where(user_id: user.id, like: 1)
   end
