fe0fabe3d93678664a0bc471e3745f44ee34acdb
kaelaclement <stephanie.kaela@gmail.com>
2019-05-22 20:05:26 -0700

    render new reviews as json

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,9 @@
 class ReviewsController < ApplicationController
-  def create
-    @recipe = Recipe.find_by(id: params[:recipe_id])
-    review = current_user.reviews.build(review_params)
-    review.recipe = @recipe
 
-    if review.save
-      redirect_to recipe_path(review.recipe)
-    else
-      flash[:alert] = "Please like or comment to submit a review"
-      redirect_to recipe_path(@recipe)
-    end
+  def create
+    recipe = Recipe.find_by(id: params[:recipe_id])
+    @review = current_user.reviews.create(review_params)
+    render json: @review
   end
 
   private
