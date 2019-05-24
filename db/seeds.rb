5f3a70db0d7ee8517be4dff71cdcd18ac9b059b4
kaelaclement <stephanie.kaela@gmail.com>
2019-05-24 15:36:01 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,18 @@ lornas_recipe = Recipe.create(author: lorna, title: "bagel", ingredients: "bagel
 rhiannons_recipe = Recipe.create(author: rhiannon, title: "buffalo cauliflower", ingredients: "1 head cauliflower, 1 cup hot sauce, 1 cup flour, 2 eggs, salt", instructions: "Prep cauliflower. Batter cauliflower. Bake cauliflower at a temperature. Sauce the cauliflower. Bake again. Enjoy.")
 
 Review.create([
+  # reviews without likes
   {user: kaela, recipe: lornas_recipe, comment: "did we need a recipe?"},
+  {user: kate, recipe: lornas_recipe, comment: "would love to see a more special bagel recipe"},
+  {user: kate, recipe: kaelas_recipe2, comment: "would love something more specific"},
+
+  #reviews with likes and comments
   {user: lorna, recipe: kaelas_recipe, like: 1, comment: "i love this stuff!"},
-  {user: lorna, recipe: kaelas_recipe2, like: 1},
   {user: rhiannon, recipe: kaelas_recipe2, like: 1, comment: "i make this all the time!!"},
+  {recipe: rhiannons_recipe, user: kate, like: 1, comment: "i fried them instead of baking and it was perfect"},
+
+  #reviews with likes and no comments
+  {user: lorna, recipe: kaelas_recipe2, like: 1},
   {recipe: rhiannons_recipe, user: kaela, like: 1},
-  {recipe: rhiannons_recipe, user: lorna, like: 1},
-  {recipe: rhiannons_recipe, user: kate, like: 1, comment: "i fried them instead of baking and it was perfect"}
+  {recipe: rhiannons_recipe, user: lorna, like: 1}
 ])
