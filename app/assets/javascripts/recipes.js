bf8691fdc69b16e2577e15ce9e82413f96eb3a4f
kaelaclement <stephanie.kaela@gmail.com>
2019-05-24 17:16:01 -0700

    create Recipe class to append formatted links to DOM

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -3,10 +3,25 @@ const attachListeners = () => {
 };
 
 function getUserRecipes() {
+  $('div#userRecipes').html('');
   const userId = $('button#getRecipes').attr('data-user-id');
-  $.get(`/users/${userId}/recipes`, function(data) {
-    console.log(data);
+  $.getJSON(`/users/${userId}/recipes`, function(data) {
+    data.forEach(r => {
+      let recipe = new Recipe(r.title)
+      let recipeHtml = recipe.recipeLink()
+      $('div#userRecipes').append(recipeHtml)
+    });
   });
+};
+
+class Recipe {
+  constructor(title) {
+    this.title = title;
+  };
+
+  recipeLink() {
+    return `<p><a href=#>${this.title}</a></p>`
+  };
 };
 
 $(function() {
