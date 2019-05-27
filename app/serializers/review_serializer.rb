82453e72e064612e94d3c3afa7f7a503cd1b1188
kaelaclement <stephanie.kaela@gmail.com>
2019-05-26 17:00:14 -0700

    add custom user_name  attribute for Reviews

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,10 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :like, :created_at, :user_id
+  attributes :id, :comment, :like, :created_at, :user_name
   belongs_to :user
   belongs_to :recipe
+
+  def user_name
+    object.user.name
+  end
+
 end
