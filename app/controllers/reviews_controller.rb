bb0a85f129be850a681247d6a80ba1fafcd8e3f1
kaelaclement <stephanie.kaela@gmail.com>
2019-05-27 16:35:21 -0700

    add new review controller action and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,10 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    render layout: false
+  end
+
   def create
     recipe = Recipe.find_by(id: params[:recipe_id])
     @review = current_user.reviews.create(review_params)
