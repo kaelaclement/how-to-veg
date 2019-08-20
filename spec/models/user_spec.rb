e8a3ec11fd2115c9ccae78d85c3861ea17a51d39
kaelaclement <stephanie.kaela@gmail.com>
2019-08-20 12:39:08 +0100

    add associations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe User, type: :model do
     subject { User.create(name: "John") }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  describe 'associations' do
+    it { should have_many(:reviews) }
+    it { should have_many(:authored_recipes).class_name('Recipe') }
+    it { should have_many(:reviewed_recipes).through(:reviews).source(:recipe) }
+  end
   
 
 end
