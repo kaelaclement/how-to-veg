997bb39652ecad50e6b2c3093c0533e2095e8203
kaelaclement <stephanie.kaela@gmail.com>
2019-08-20 12:45:25 +0100

    add validation tests to recipes

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:ingredients) }
+    it { should validate_presence_of(:instructions) }
+  end
+
 end
