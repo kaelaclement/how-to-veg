cb439f59b1febfa19168c53d9821ba970021bc8f
kaelaclement <stephanie.kaela@gmail.com>
2019-10-10 13:25:06 -0700

    add test for recipe search

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe Recipe, type: :model do
     it { should have_many(:users).through(:reviews) }
   end
 
+  it "can search for recipes by title" do
+    @user = User.create(name: "Kate", email: "kate@veggie.com", password: "password")
+    @recipe_one = Recipe.create(title: "Avocado Toast", ingredients: "bread of choice, butter, 1/2 an avocado, lime juice, salt and pepper to taste", instructions: "Toast your bread, spread with butter, mash avocado and apply half to each slice of bread. sprinkle with lime juice, salt, and pepper. Enjoy!", author: user)
+    @recipe_two = Recipe.create(title: "Something Else", ingredients: "other ingredients", instructions: "Make it.", author: user)
+
+    expect(Recipe.find_in_title("avocado").first.title).to eq(@recipe_one.title)
+  end
+
 end
