64997554664bcf3ed240486381b80aad3c60dd92
kaelaclement <stephanie.kaela@gmail.com>
2019-11-10 19:57:58 -0800

    add user signup test

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature "SignUp", type: :feature do
     expect(page).to have_content("Password")
     expect(page).to have_content("Password confirmation")
   end
+
+  it 'successfully signs up a valid user' do
+    visit '/users/new'
+    fill_in("user[name]", with: "Leah")
+    fill_in("user[email]", with: "leah@howtoveg.com")
+    fill_in("user[password]", with: "password")
+    fill_in("user[password_confirmation]", with: "password")
+    click_button('Sign Up')
+    expect(current_path).to eq('/users/1')
+    expect(page).to have_content("Leah")
+  end
 end
